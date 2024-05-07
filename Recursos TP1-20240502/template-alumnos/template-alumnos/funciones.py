43f943c7674aae44a40e439e9c413fa1e426b25b
Tiagobusso <132584505+Tiagobusso@users.noreply.github.com>
2024-05-07 12:11:50 -0300

    Update funciones.py
    Fix test unitario

--- Recursos TP1-20240502/template-alumnos/template-alumnos/funciones.py
+++ Recursos TP1-20240502/template-alumnos/template-alumnos/funciones.py
@@ -4,10 +4,9 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import time
 
+def leer_archivo(input_file_path):
 
-def leer_archivo(Args_file_path):
-
-    f = open(Args_file_path, 'r')
+    f = open(input_file_path, 'r')
     n = int(f.readline()) # cantidad de elementos del sistema
     m = int(f.readline()) # cantidad de links del sistema
     W = np.zeros(shape=(n,n)) #
@@ -43,80 +42,94 @@ def dibujarGrafo(W, print_ejes=True):
 
 
 def calcularGrado(W):
-    '''Calcula el grado de cada pagina j de una matriz de conectividad W
+    '''
+    Calcula el grado de cada pagina j de una matriz de conectividad W
 
-    Args:
-        W: matriz de conectividad entre paginas i y paginas j.
+    Input:
+    W: matriz de conectividad entre paginas i y paginas j.
 
-    Returns:
-        grados: vector n-dimensional cuyos elementos son los grados de las paginas    '''
+    Output:
+    grados: vector n-dimensional cuyos elementos son los grados de las paginas
+    '''
     npages = W.shape[0] # cantidad de paginas
     grados = np.zeros(npages) # array n-dim con los grados de cada pagina j
+
     for j in range(npages):
-     grados[j] = sum(W[i][j] for i in range(npages)) 
-     
+      for i in range(npages):
+        grados[j] += W[i][j] # en el enunciado, grados[j] = c_j
+
     return grados
 
-def matrizPuntajes(W):
-    '''Calcula la matriz de puntajes R = WD donde W es la matriz de conectividad, D es la matriz diagonal con grados 'normalizados'
+    # funcionando
 
-    Args:
-        W: matriz de conectividad entre paginas i y paginas j.
-        
-    Returns: 
-        R: matriz de puntajes'''
+def matrizPuntajes(W):
+    '''
+    Calcula la matriz de puntajes R = WD donde W es la matriz de conectividad, D es la matriz diagonal con grados 'normalizados'
+
+    W: matriz de conectividad entre paginas i y paginas j.
+    '''
     npages = W.shape[0]
     D = np.eye(npages) # genero D
     grados = calcularGrado(W) # genero array de grados
-    for j in range(npages):
-        D[j][j] = 0 if grados[j] == 0 else D[j][j] / grados[j]
 
-    return W@D
+    for j in range(npages):
+      if grados[j] == 0: # evitamos la division por 0
+        D[j][j] = 0
+      else:
+        D[j][j] /= grados[j]
+
+    R = W @ D
+
+    return R
+
+    # funcionando
 
 def naveganteAleatorio(W, p):
-    '''Calcula la probabilidad de salto entre paginas para γ (gamma) != 1
+    '''
+    Calcula la probabilidad de salto entre paginas para γ (gamma) != 1
 
-    Args:
-        W: matriz de conectividad
-        p: probabilidad de seguir un link de pagina
+    Input:
+    W: matriz de conectividad
+    p: probabilidad de seguir un link de pagina
 
-    Returns:
-        ez_T: matriz cuyas filas son iguales a z^T    '''
-    
+    Output:
+    ez_T: matriz cuyas filas son iguales a z^T
+    '''
     grados = calcularGrado(W) # defino vector de grados para calcular de z
     npages = W.shape[0]
-    e, z = np.ones((npages,1)), np.ones((npages,1)) # crea una matriz de unos de dimension (npages x 1) (vector columna)
+    e = np.ones((npages,1)) # crea una matriz de unos de dimension (npages x 1) (vector columna)
+    z = np.ones((npages,1))
+
     for j in range(npages): # cómputo de z
-        z[j] = z[j]/npages if grados[j] == 0 else (1-p)/npages
+        if grados[j] == 0:
+          z[j] /= npages
+        else:
+          z[j] = (1 - p)/npages
+
     ez_T = e@(z.T)
-    
     return ez_T
 
 def factLU(A):
-    """Descompone matriz A en LU
 
-    Args:
-        A (_type_): _description_
-
-    Returns:
-        _type_: _description_"""
-    
     m=A.shape[0]
     n=A.shape[1]
     Ac = A.copy()
+
     if m!=n:
         print('Matriz no cuadrada')
         return
+
     matriz_factores = np.zeros((n,n))  # lista que almacena las matrices factores
-    
+
     for j in range(n): # j = columna
         factores = np.zeros((n,n))  # creo una matriz identidad del tamaño de A
         for i in range(j+1, n): # i = fila
             factores[i, j] = - Ac[i, j] / Ac[j, j]  # calcular el coeficiente factores para cada fila i
             Ac[i, j:] += factores[i, j] * Ac[j, j:]  # aplicar la operación de eliminación gaussiana a la fila i
         matriz_factores += factores  # agregar la matriz factores a la lista.
-        
+
     Ac -= matriz_factores
+
     L = np.tril(Ac,-1) + np.eye(A.shape[0])
     U = np.triu(Ac)
 
@@ -124,27 +137,34 @@ def factLU(A):
 
 
 def sort_rnk(arr):
-    '''Ordena el array dado usando selection sort, es decir que recorre el arreglo buscando siempre el minimo y lo pone delante.
-    El factor de ordenanza es el segundo elemento de la tupla.
-    Complejidad: O(n^2)
+  '''
+  Ordena el array dado usando selection sort, es decir que recorre el arreglo buscando siempre el minimo y lo pone delante.
+  El factor de ordenanza es el segundo elemento de la tupla.
+  Complejidad: O(n^2)
 
-    Args:
-        arr: El arreglo a ordenar.
-        el parametro de entrada es de tipo: [(index,score)]
-    Returns:
-        Un nuevo arreglo ordenado.'''
-    rnk = []
-    while arr:
-        max_score = arr[0][1]  # define el puntaje máximo como el puntaje del primer elemento de la lista
-        max_index = 0
-        for i in range(len(arr)):  # itera sobre los índices del arreglo
-            if arr[i][1] > max_score:  # si el score es mayor actualiza index y score maximo
-                max_score = arr[i][1]
-                max_index = i
-        rnk.append(arr[max_index][0])  # agrega el índice del puntaje máximo a la lista de rangos
-        del arr[max_index]  # elimina el elemento con el puntaje máximo de la lista
-        
-    return rnk
+  Input:
+      arr: El arreglo a ordenar.
+      el parametro de entrada es de tipo: [(index,score)]
+  Output:
+      Un nuevo arreglo ordenado.
+
+  '''
+
+  rnk = []
+  while arr:
+      max_score = arr[0][1]  # define el puntaje máximo como el puntaje del primer elemento de la lista
+      max_index = 0
+      for i in range(len(arr)):  # itera sobre los índices del arreglo
+          if arr[i][1] > max_score:  # si el score es mayor actualiza index y score maximo
+              max_score = arr[i][1]
+              max_index = i
+      rnk.append(arr[max_index][0])  # agrega el índice del puntaje máximo a la lista de rangos
+      del arr[max_index]  # elimina el elemento con el puntaje máximo de la lista
+  return rnk
+
+
+
+
 
 def calcularRanking(M, p): # ingresa la matriz W de conectividad
     npages = M.shape[0]
@@ -153,15 +173,19 @@ def calcularRanking(M, p): # ingresa la matriz W de conectividad
     R = matrizPuntajes(M)
     A = np.eye(npages) - p*R
     b = np.ones((npages, 1)) # vector columna e
-    L, U = factLU(A) 
-    x = sp.linalg.solve_triangular(U, sp.linalg.solve_triangular(L,b, lower=True))
-    norma = np.linalg.norm(x,1)
-    scr = x/norma
+    L, U = factLU(A)
+    y = sp.linalg.solve_triangular(L,b, lower=True)
+    x = sp.linalg.solve_triangular(U,y)
     indexAndScore = []
+    norma = sp.linalg.norm(x,1)
+    scr = x/norma
+
     for i in range(npages):
        indexAndScore.append((i, scr[i]))
+
     rnk = sort_rnk(indexAndScore)
-    
+
+    print(rnk, scr)
     return rnk, scr
 
 def obtenerMaximoRankingScore(M, p):
@@ -171,7 +195,6 @@ def obtenerMaximoRankingScore(M, p):
     output = np.max(scr)
 
     return output
-
 
 #####################################
 #                ANALISIS CUANTITATIVO DE DATOS               #
